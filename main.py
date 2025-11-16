1e36ee1ad3536f9d80bd6b6a0a088615ded3ea59
ALADETAN-IFE <famousfotune123@gmail.com>
2025-11-16 23:30:09 +0100

    feat: Implement basic FastAPI application with item management endpoints

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get("/")
 def read_root():
-    return {"Hello World"}
+    return {"welcome" : "Hello World"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return {"message": item + " was added successfull", "data": items}
+
+
+@app.get("/items")
+def read_items():
+    return items
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int) -> str:
+    item = items[item_id]
+    return item
+
+
